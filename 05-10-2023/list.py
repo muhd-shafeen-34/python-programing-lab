5efa93cfe862ac28facae4e78882a204456e2e05
muhd-shafeen-34 <145667235+muhd-shafeen-34@users.noreply.github.com>
2023-10-05 16:19:03 +0530

    Add files via upload

---
+++ 05-10-2023/list.py
@@ -0,0 +1,8 @@
+#print the positive integers to another list
+a =[1,2,-1,-2,-3,4,5,6]
+lst=[]
+for i in a:
+    if i>0:
+        lst.append(i)
+print(lst)
+        
---
+++ 05-10-2023/vowels.py
@@ -0,0 +1,11 @@
+#print all the vowels in a string
+a = str(input("enter a string: "))
+b = ["A","E","I","O","U"]
+for i in a:
+    if i.upper() in b:
+        print(i)
+    
+
+
+        
+
